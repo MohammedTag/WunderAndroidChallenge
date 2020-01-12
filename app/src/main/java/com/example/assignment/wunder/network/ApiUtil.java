6a2b64f65e094b8639c6e38705938f1ae163c95e
Mohammed tag <mohamed.taj@thed.io>
2020-01-12 02:18:50 +0200

    - clean up commented code - editing min sdk

--- app/src/main/java/com/example/assignment/wunder/network/ApiUtil.java
+++ app/src/main/java/com/example/assignment/wunder/network/ApiUtil.java
@@ -2,10 +2,6 @@ package com.example.assignment.wunder.network;
 
 public class ApiUtil {
 
-
-    //public static final String BASE_URL = "https://fake-poi-api.mytaxi.com/";
-    //public static final String BASE_URL = "https://raw.githubusercontent.com/";
-
     public static final String BASE_URL =  "https://s3.eu-central-1.amazonaws.com/wunderfleet-recruiting-dev/";
 
     public static GetDataService getDataService(){
